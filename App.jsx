b7f13722db2e94ecfe9950291a0696c6ec53475f
yjb23tech <yjb23tech@gmail.com>
2023-11-17 23:48:26 +0000

    Yes Or No

--- App.jsx
+++ App.jsx
@@ -1,5 +1,11 @@
 import {createRoot} from "react-dom/client";
 
+const App = () => {
+    return (
+        <h1>Sanity Check</h1>
+    )
+}
+
 const container = document.getElementById("root")
 const root = createRoot(container)
 root.render(<App />)

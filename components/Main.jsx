ecf7d3cabfac6bc54cfe8e82bfdfbe4e6d3b597f
yjb23tech <yjb23tech@gmail.com>
2023-11-18 00:24:41 +0000

    Smooth so far

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
             <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
-                <li>Was oriignally created by Jordan Walker</li>
+                <li>Was orignally created by Jordan Walker</li>
                 <li>Has well over 100k stars on Github</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>

1b875ca503377fc085492e874c8ae0961494f612
yjb23tech <yjb23tech@gmail.com>
2023-11-18 00:16:42 +0000

    Learning so much

---
+++ components/Main.jsx
@@ -0,0 +1,17 @@
+const Main = () => {
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was oriignally created by Jordan Walker</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+
+    );
+};
+
+export default Main;
